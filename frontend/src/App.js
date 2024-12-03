f36db203566384e43f3959051bbb4615e5a0f94b
pufington <ganesh289katta@gmail.com>
2024-12-03 11:05:03 -0600

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,89 +1,68 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from "react-router-dom";
-import Home from './Home';
-import LogIn from './logIn.js';
-import CreateAccount from './CreateAccount.js';
-import SchedulingAppt from './schedulingAppt.js';
-import ViewMedHist from './ViewMedHist.js';
-import DocHome from './DocHome.js';
-import ViewOneHistory from './ViewOneHistory.js';
-import Settings from './Settings.js';
-import DocSettings from './DocSettings.js';
-import PatientsViewAppt from './PatientsViewAppt.js';
-import NoMedHistFound from './NoMedHistFound.js';
-import DocViewAppt from './DocViewAppt.js';
-import MakeDoc from './MakeDoc.js';
-import Diagnose from './Diagnose.js';
-import ShowDiagnoses from './ShowDiagnoses.js';
+import Home from "./Home";
+import LogIn from "./logIn.js";
+import CreateAccount from "./CreateAccount.js";
+import SchedulingAppt from "./schedulingAppt.js";
+import ViewMedHist from "./ViewMedHist.js";
+import DocHome from "./DocHome.js";
+import ViewOneHistory from "./ViewOneHistory.js";
+import Settings from "./Settings.js";
+import DocSettings from "./DocSettings.js";
+import PatientsViewAppt from "./PatientsViewAppt.js";
+import NoMedHistFound from "./NoMedHistFound.js";
+import DocViewAppt from "./DocViewAppt.js";
+import MakeDoc from "./MakeDoc.js";
+import Diagnose from "./Diagnose.js";
+import ShowDiagnoses from "./ShowDiagnoses.js";
 
 export default function App() {
-  let [component, setComponent] = useState(<LogIn />)
-  useEffect(()=>{
+  const [component, setComponent] = useState(<LogIn />);
+
+  useEffect(() => {
     fetch("https://project-tjlu.onrender.com/userInSession")
-      .then(res => res.json())
-      .then(res => {
-      let string_json = JSON.stringify(res);
-      let email_json = JSON.parse(string_json);
-      let email = email_json.email;
-      let who = email_json.who;
-      if(email === ""){
-        setComponent(<LogIn />)
-      }
-      else{
-        if(who==="pat"){
-          setComponent(<Home />)
+      .then((res) => res.json())
+      .then((res) => {
+        const { email, who } = res;
+        if (email === "") {
+          setComponent(<LogIn />);
+        } else {
+          if (who === "pat") {
+            setComponent(<Home />);
+          } else {
+            setComponent(<DocHome />);
+          }
         }
-        else{
-          setComponent(<DocHome />)
-        }
-      }
-    });
-  }, [])
+      })
+      .catch((error) => {
+        console.error("Error fetching user session:", error);
+        setComponent(<LogIn />);
+      });
+  }, []);
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/NoMedHistFound">
-            <NoMedHistFound />
-          </Route>
-          <Route path="/MakeDoc">
-            <MakeDoc />
-          </Route>
-          <Route path="/Settings">
-            <Settings />
-          </Route>
-          <Route path="/MedHistView">
-            <ViewMedHist />
-          </Route>
-          <Route path="/scheduleAppt">
-            <SchedulingAppt />
-          </Route>
-          <Route path="/showDiagnoses/:id" render={props=><ShowDiagnoses {...props} />} />
-          <Route path="/Diagnose/:id" render={props=><Diagnose {...props} />} />
-          <Route name="onehist" path="/ViewOneHistory/:email" render={props=><ViewOneHistory {...props} />}/>
-          <Route path="/Home">
-            <Home />
-          </Route>
-          <Route path="/createAcc">
-            <CreateAccount />
-          </Route>
-          <Route path="/DocHome">
-            <DocHome />
-          </Route>
-          <Route path="/PatientsViewAppt">
-            <PatientsViewAppt />
-          </Route>
-          <Route path="/DocSettings">
-            <DocSettings />
-          </Route>
-          <Route path="/ApptList">
-            <DocViewAppt />
-          </Route>
-          <Route path="/">
+          <Route exact path="/NoMedHistFound" component={NoMedHistFound} />
+          <Route exact path="/MakeDoc" component={MakeDoc} />
+          <Route exact path="/Settings" component={Settings} />
+          <Route exact path="/MedHistView" component={ViewMedHist} />
+          <Route exact path="/scheduleAppt" component={SchedulingAppt} />
+          <Route exact path="/showDiagnoses/:id" component={ShowDiagnoses} />
+          <Route exact path="/Diagnose/:id" component={Diagnose} />
+          <Route exact path="/ViewOneHistory/:email" component={ViewOneHistory} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/createAcc" component={CreateAccount} />
+          <Route exact path="/DocHome" component={DocHome} />
+          <Route exact path="/PatientsViewAppt" component={PatientsViewAppt} />
+          <Route exact path="/DocSettings" component={DocSettings} />
+          <Route exact path="/ApptList" component={DocViewAppt} />
+          <Route exact path="/">
             {component}
           </Route>
         </Switch>
